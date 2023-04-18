97b29f3f824e86b3b588d4ef60c2b4aba4066007
baz3004 <japhetbazungula01@gmail.com>
2023-04-18 09:26:14 +0100

    dernier version

--- authentification/forms.py
+++ authentification/forms.py
@@ -3,10 +3,14 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='Nom d’utilisateur')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+    username = forms.CharField ( max_length=63, label='',widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(max_length=63,label='',widget=forms.PasswordInput(attrs={'placeholder': 'Mot de passe'}))
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Name'}),
+            'email': forms.EmailField(),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Votre postNom '}),
+        }
